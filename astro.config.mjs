9d43b6725fe3d5fc3f7164b9196e17a1db9e99d1
Meta-Writing Ecology <tzuyuanhuang.tw@gmail.com>
2026-06-04 18:49:50 +0800

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://metawritingecology.org",
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [mdx(), sitemap()]
+});
